0c79c139eaaafcef21407c1dbb91cf7b0ac54818
SvetlanaTeryaeva <sveteok@gmail.com>
2025-09-15 09:08:51 +0300

    js-and-ts arrays a8 (done)

--- js-and-ts/8-arrays/a8-reverse-words/a8.js
+++ js-and-ts/8-arrays/a8-reverse-words/a8.js
@@ -1,9 +1,26 @@
 /*
-Create a function reverseWords that reverses each word in a string, but does not reverse the order of the words in the string.
+Create a function reverseWords that reverses each word in a string, 
+but does not reverse the order of the words in the string.
 
 const sentence = "this is a short sentence";
 const reversed = reverseWords(sentence);
 console.log(reversed); // prints "siht si a trohs ecnetnes"
 */
 
-const reverseWords = (str) => {};
+const reverseWords = (str) => {
+  const words = str.split(" ");
+  const reverseWords = [];
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    let reversedWord = "";
+    for (let j = word.length - 1; j >= 0; j--) {
+      reversedWord += word[j];
+    }
+    reverseWords.push(reversedWord);
+  }
+  return reverseWords.join(" ");
+};
+
+const sentence = "this is a short sentence";
+const reversed = reverseWords(sentence);
+console.log(reversed); // prints "siht si a trohs ecnetnes"

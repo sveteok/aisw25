00f364ecba79bfb8c01c328130ce99ba0786c7e7
SvetlanaTeryaeva <sveteok@gmail.com>
2025-09-15 16:15:49 +0300

    js-and-ts array methods e10 (done)

---
+++ js-and-ts/11-array-methods/e10-object-arrays/e10.js
@@ -0,0 +1,76 @@
+/*
+Given the following array of employee objects:
+
+const users: Person[] = [
+  { firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resident' },
+  { firstName: 'Chloe', lastName: 'Alnaji', role: 'Full Stack Resident' },
+  { firstName: 'Jonathan', lastName: 'Baughn', role: 'Enterprise Instructor' },
+  { firstName: 'Michael', lastName: 'Herman', role: 'Lead Instructor' },
+  { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
+  { firstName: 'Wes', lastName: 'Reid', role: 'Instructor'},
+  { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor'}
+];
+Use the array methods to create new arrays.
+
+1. List the last names of all employees
+2. List all employee objects that are Full Stack Residents
+3. List all Instructors' full names (e.g., Wes Reid)
+*/
+const users = [
+  { firstName: "Bradley", lastName: "Bouley", role: "Full Stack Resident" },
+  { firstName: "Chloe", lastName: "Alnaji", role: "Full Stack Resident" },
+  { firstName: "Jonathan", lastName: "Baughn", role: "Enterprise Instructor" },
+  { firstName: "Michael", lastName: "Herman", role: "Lead Instructor" },
+  { firstName: "Robert", lastName: "Hajek", role: "Full Stack Resident" },
+  { firstName: "Wes", lastName: "Reid", role: "Instructor" },
+  { firstName: "Zach", lastName: "Klabunde", role: "Instructor" },
+];
+
+//1. List the last names of all employees
+const lastNames = users.map((user) => user.lastName);
+console.log(lastNames);
+/*
+Output:
+[
+  'Bouley',   'Alnaji',
+  'Baughn',   'Herman',
+  'Hajek',    'Reid',
+  'Klabunde'
+]
+*/
+
+//2. List all employee objects that are Full Stack Residents
+const fullStackResidents = users.filter(
+  (user) => user.role === "Full Stack Resident"
+);
+console.log(fullStackResidents);
+/*
+Output:
+[
+  {
+    firstName: 'Bradley',
+    lastName: 'Bouley',
+    role: 'Full Stack Resident'
+  },
+  {
+    firstName: 'Chloe',
+    lastName: 'Alnaji',
+    role: 'Full Stack Resident'
+  },
+  {
+    firstName: 'Robert',
+    lastName: 'Hajek',
+    role: 'Full Stack Resident'
+  }
+]
+*/
+
+//3. List all Instructors' full names (e.g., Wes Reid)
+const instructorsFullNames = users
+  .filter((user) => user.role.includes("Instructor"))
+  .map((user) => `${user.firstName} ${user.lastName}`);
+console.log(instructorsFullNames);
+/*
+Output
+[ 'Jonathan Baughn', 'Michael Herman', 'Wes Reid', 'Zach Klabunde' ]
+*/

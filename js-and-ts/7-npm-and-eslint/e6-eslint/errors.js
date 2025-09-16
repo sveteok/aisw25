0221b7e3d14a84b9d9933bb64803b9075009a807
sveteok <sveteok@gmail.com>
2025-09-16 15:48:51 +0300

    js-and-ts npm and eslint e6 (update)

--- js-and-ts/7-npm-and-eslint/e6-eslint/errors.js
+++ js-and-ts/7-npm-and-eslint/e6-eslint/errors.js
@@ -1,17 +1,25 @@
-/* eslint-disable no-unused-vars */
-const name = "Han";
+// eslint no-unused-vars: "error"
+// const name = "Han";
+//eslint "no-const-assign": "error"
 // name = "Solo";
 
 let planet = "Tatooine";
+// let planet = "Hoth"; //Parsing error
+// eslint no-unused-vars: "error"
 planet = "Hoth";
+console.log(planet);
 
+// eslint "no-undef": "error"
 // console.log(result);
 
+// eslint no-unused-vars: "error"
 // var ship = "Falcon";
 
 const speed = 0;
+// if (speed == "0") { //eslint eqeqeq: "error"
 if (speed === "0") {
   console.log("stopped");
 }
 
+// eslint no-unused-vars: "error"
 // const unused = 123;

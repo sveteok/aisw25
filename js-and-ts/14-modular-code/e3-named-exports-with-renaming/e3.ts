d74093d6c370a47fa6ff4bad5d03967d62a3d899
SvetlanaTeryaeva <sveteok@gmail.com>
2025-09-22 14:07:16 +0300

    js-and-ts modular-code e1-e3 (done)

---
+++ js-and-ts/14-modular-code/e3-named-exports-with-renaming/e3.ts
@@ -0,0 +1,5 @@
+/*
+Create a module called utilities.ts that exports several utility functions (like formatDate, validateEmail, generateId).
+
+Import these functions with different names in your main file and test them.
+*/

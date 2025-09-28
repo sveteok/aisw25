24b4411aa1ad51b908563afe0cb8532acc57fb82
SvetlanaTeryaeva <sveteok@gmail.com>
2025-09-28 15:32:07 +0300

    js-and-ts 15-file-i (clean code by Prettierfix)

--- js-and-ts/15-file-i_o/e1-read-write-file/e1.ts
+++ js-and-ts/15-file-i_o/e1-read-write-file/e1.ts
@@ -14,58 +14,58 @@ Joulu on taas, joulu on taas, kattilat täynnä puuroo. Nyt sitä saa, nyt sitä
 Joulu on taas, joulu on taas, voi, kuinka meill on hauskaa! Lapsilla on, lapsilla on aamusta iltaan hauskaa.
 */
 
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-import { Transform } from "node:stream";
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { Transform } from 'node:stream';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const inputFilePath = path.join(__dirname, "../../textFile.txt");
-const outputFilePath = path.join(__dirname, "../../alteredTextFile.txt");
+const inputFilePath = path.join(__dirname, '../../textFile.txt');
+const outputFilePath = path.join(__dirname, '../../alteredTextFile.txt');
 
 // fs.createReadStream(filePath, "utf-8").on("data", (txt) => console.log(txt));
 
 const replaceStream = new Transform({
-  transform(chunk, _, callback) {
-    const text = chunk.toString();
-    const parts = text.split(/(\b)/);
+	transform(chunk, _, callback) {
+		const text = chunk.toString();
+		const parts = text.split(/(\b)/);
 
-    const processed = parts
-      .map((word: string) => {
-        if (/^\w+$/u.test(word)) {
-          if (/^joulu$/i.test(word)) {
-            console.log("kinkku");
-            return "kinkku";
-          }
-          if (/^lapsilla$/i.test(word)) {
-            console.log("poroilla");
-            return "poroilla";
-          }
-          console.log(word);
-        }
-        return word;
-      })
-      .join("");
+		const processed = parts
+			.map((word: string) => {
+				if (/^\w+$/u.test(word)) {
+					if (/^joulu$/i.test(word)) {
+						console.log('kinkku');
+						return 'kinkku';
+					}
+					if (/^lapsilla$/i.test(word)) {
+						console.log('poroilla');
+						return 'poroilla';
+					}
+					console.log(word);
+				}
+				return word;
+			})
+			.join('');
 
-    this.push(processed);
-    callback();
-  },
+		this.push(processed);
+		callback();
+	},
 });
 
-const readStream = fs.createReadStream(inputFilePath, "utf-8");
-const writeStream = fs.createWriteStream(outputFilePath, "utf-8");
+const readStream = fs.createReadStream(inputFilePath, 'utf-8');
+const writeStream = fs.createWriteStream(outputFilePath, 'utf-8');
 
 readStream
-  .pipe(replaceStream)
-  .pipe(writeStream)
-  .on("finish", () => {
-    console.log(`Done. Altered text written to ${outputFilePath}`);
-  })
-  .on("error", (err) => {
-    console.error("Error:", err);
-  });
+	.pipe(replaceStream)
+	.pipe(writeStream)
+	.on('finish', () => {
+		console.log(`Done. Altered text written to ${outputFilePath}`);
+	})
+	.on('error', (err) => {
+		console.error('Error:', err);
+	});
 
 /*
 alteredTextFile.txt
--- js-and-ts/15-file-i_o/e2-find-specific-forecast/e2.ts
+++ js-and-ts/15-file-i_o/e2-find-specific-forecast/e2.ts
@@ -12,30 +12,30 @@ const forecast: Forecast = {
 Then retrieve the data and modify the temperature of that forecast.
 Lastly, save the change back to the file.
 */
-import fs from "fs";
+import fs from 'fs';
 
 interface Forecast {
-  day: string;
-  temperature: number;
-  cloudy: boolean;
-  sunny: boolean;
-  windy: boolean;
+	day: string;
+	temperature: number;
+	cloudy: boolean;
+	sunny: boolean;
+	windy: boolean;
 }
 
 const forecast: Forecast = {
-  day: "monday",
-  temperature: 20,
-  cloudy: true,
-  sunny: false,
-  windy: false,
+	day: 'monday',
+	temperature: 20,
+	cloudy: true,
+	sunny: false,
+	windy: false,
 };
 
 const writeData: string = JSON.stringify(forecast, null, 4);
-fs.writeFileSync("../../forecast_data.json", writeData, "utf8");
+fs.writeFileSync('../../forecast_data.json', writeData, 'utf8');
 
-const readData: string = fs.readFileSync("../../forecast_data.json", "utf8");
+const readData: string = fs.readFileSync('../../forecast_data.json', 'utf8');
 const allForecasts: Forecast = JSON.parse(readData);
 
 const updateForecast = { ...allForecasts, temperature: 22 };
 const writeUpdatedData: string = JSON.stringify(updateForecast, null, 4);
-fs.writeFileSync("../forecast_data.json", writeUpdatedData, "utf8");
+fs.writeFileSync('../forecast_data.json', writeUpdatedData, 'utf8');
